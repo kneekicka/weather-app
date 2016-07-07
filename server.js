4fc48b00d1197e4124c1ec3445c8605b91520993
kneekicka <kneekicka@users.noreply.github.com>
2016-07-07 09:52:19 +0300

    Comments deletion

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-// server.js
+var http = require('http');
+var express  = require('express');
+var app      = express();
 
-    // set up ========================
-    var http = require('http');
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
+app.use(express.static(__dirname + '/public'));
 
-    // configuration ================
+app.get('/', function(req, res) {
+    res.sendfile('/public/index.html');
+});
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location
-
-    // application -------------------------------------------------------------
-    app.get('/', function(req, res) {
-        res.sendfile('/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-
-    // listen (start app with node server.js) ======================================
-    app.listen(3000);
-    console.log("App listening on port 3000");
+app.listen(3000);
+console.log("App listening on port 3000");
 
